b668f36828def2098285dc01ced4e9423d9d29ec
graysquirrel <fritzebner@gmail.com>
2021-06-18 17:21:21 -0400

    updated to new dir structure

--- addBOPfiles.R
+++ addBOPfiles.R
@@ -1,42 +1,48 @@
 library(openxlsx)
 
-# go to the folder.  assume below this folder is 'newmice' with new bop files, and 'masterfile' where the big xls is.
-setwd("/Users/fritzebner/Documents/R/Grace/temp/graceBOPprocessing/newmice/")
-
-# make a backup copy of master file
-nowstamp <- format(Sys.time(), "%Y-%m-%d_%H_%M_%s")
-masterfilename <- "../masterfiles/BlastAnimalOutcomes.xlsx"
-file.copy(masterfilename, paste0(sub('\\.xlsx$','', masterfilename),nowstamp,".xlsx"))
+# assume you are running this script at the level where subdirectories masterfiles and "BOP outputs" exists
+bopfilefolder <- "BOP outputs"
 
 # only take files > 210616
-newfiles <- list.files(pattern="\\BOP000.csv$")
+newfiles <- list.files(path=bopfilefolder, pattern="\\BOP000.csv$")
 newdates <- as.Date(newfiles, format = "%y%m%d")
 newfiles <- newfiles[newdates > as.Date("2021-06-16")]
-for (f in newfiles) 
+if (length(newfiles) > 0) 
 {
-  print(f)
-  # get date from filename
-  thedate <- as.Date(f, format = "%y%m%d")
-  # read in the bop file
-  df <- read.csv(f, header = FALSE, stringsAsFactors = FALSE, fileEncoding="latin1")
-  # get psi
-  psi <- df[df$V1=="BOP","V2"]
-  psi <- trimws(psi)
-  psi <- substr(psi,start = 1, stop = (nchar(psi) - 4))
-  # get mouse number
-  mousenum <- trimws(df$V1[dim(df)[1]])
-  len <- nchar(mousenum)
-  mousenum <- substr(mousenum, start = len - 3, stop = len)
-  # read in whole workbook
-  wb <- loadWorkbook(masterfilename)
-  # get master, append new data
-  master <- read.xlsx(masterfilename, 1, detectDates = TRUE)
-  newrownum <- dim(master)[1] + 1
-  master[newrownum,"Date"] <- thedate
-  master[newrownum,"Subject"] <- mousenum
-  master[newrownum,"BOP.(psi)"] <- psi
-  writeData(wb, sheet = "Master", master, colNames = T)
-  saveWorkbook(wb, masterfilename, overwrite = T)
-  # change name of file, so its not processed twice
-  file.rename(f, paste0(sub('\\.csv$','', f),"processed.csv"))
+  # make a backup copy of master file
+  nowstamp <- format(Sys.time(), "%Y-%m-%d_%H_%M_%s")
+  masterfilename <- "masterfiles/BlastAnimalOutcomes.xlsx"
+  newmasterfilename <- paste0(sub('\\.xlsx$','', masterfilename),nowstamp,".xlsx")
+  print(paste("copying",masterfilename,"to",newmasterfilename))
+  file.copy(masterfilename, newmasterfilename)
+  for (f in paste0(bopfilefolder,"/",newfiles))
+  {
+    print(paste("processing",f))
+    # get date from filename
+    thedate <- as.Date(f, format = "%y%m%d")
+    # read in the bop file
+    df <- read.csv(f, header = FALSE, stringsAsFactors = FALSE, fileEncoding="latin1")
+    # get psi
+    psi <- df[df$V1=="BOP","V2"]
+    psi <- trimws(psi)
+    psi <- substr(psi,start = 1, stop = (nchar(psi) - 4))
+    # get mouse number
+    mousenum <- trimws(df$V1[dim(df)[1]])
+    len <- nchar(mousenum)
+    mousenum <- substr(mousenum, start = len - 3, stop = len)
+    # read in whole workbook
+    wb <- loadWorkbook(masterfilename)
+    # get master, append new data
+    master <- read.xlsx(masterfilename, 1, detectDates = TRUE)
+    newrownum <- dim(master)[1] + 1
+    master[newrownum,"Date"] <- thedate
+    master[newrownum,"Subject"] <- mousenum
+    master[newrownum,"BOP.(psi)"] <- psi
+    writeData(wb, sheet = "Master", master, colNames = T)
+    saveWorkbook(wb, masterfilename, overwrite = T)
+    # change name of file, so its not processed twice
+    newf <- paste0(sub('\\.csv$','', f),"processed.csv")
+    print(paste("renaming",f,"to",newf))
+    file.rename(f, newf)
+  }
 }
