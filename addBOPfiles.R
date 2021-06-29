3d19a3e17b179052dbb861162750e15106fb429c
graysquirrel <fritzebner@gmail.com>
2021-06-29 15:10:18 -0400

    changed from reading BlastAnimalOutcomes.xlsx to just creating an xlsx each time with all the BOP files in the right columns

--- addBOPfiles.R
+++ addBOPfiles.R
@@ -1,10 +1,13 @@
 library(openxlsx)
 
-# To run, copy the R script to the folder and run 'Rscript addBOPfiles.R'
+# To run, go to Blaster - General/BOP outputs/graceBOPprocessing and run 'Rscript addBOPfiles.R'
+
+setwd("..") # First, go up a dir.  Execution will happen in "BOP outputs"
 
 # assume you are running this script at the level where subdirectories masterfiles and "BOP outputs" exists
 bopfilefolder <- "."
-masterfilename <- "masterfiles/BlastAnimalOutcomes.xlsx"
+#masterfilename <- "masterfiles/BlastAnimalOutcomes.xlsx"
+masterfilefolder <- "masterfiles"
 
 # only take files > 210616
 newfiles <- list.files(path=bopfilefolder, pattern="\\BOP000.csv$")
@@ -15,12 +18,19 @@ if (length(newfiles) > 0)
 {
   # make a backup copy of master file
   nowstamp <- format(Sys.time(), "%Y-%m-%d_%H_%M_%s")
-  newmasterfilename <- paste0(sub('\\.xlsx$','', masterfilename),nowstamp,".xlsx")
-  print(paste("copying",masterfilename,"to",newmasterfilename))
-  file.copy(masterfilename, newmasterfilename)
+  #newmasterfilename <- paste0(sub('\\.xlsx$','', masterfilename),nowstamp,".xlsx")
+  #print(paste("copying",masterfilename,"to",newmasterfilename))
+  #file.copy(masterfilename, newmasterfilename)
   # load workbook up
-  wb <- loadWorkbook(masterfilename)
-  master <- read.xlsx(masterfilename, 1, detectDates = TRUE)
+  #wb <- loadWorkbook(masterfilename)
+  #master <- read.xlsx(masterfilename, 1, detectDates = TRUE)
+  
+  master <- data.frame(matrix(ncol = 14, nrow=0))
+  columnNames <- c("Date","Subject","injury?","BOP Input (psi)", "BOP (psi)", 
+                   "Outcome", "Measures (Experiments)","48h NS", "7 day NS",
+                   "Actual Driver Pressure", "Still have", "","Filename", "Notes")
+  colnames(master) <- columnNames
+  
   for (f in newfiles)
   {
     bopfilepath <- paste0(bopfilefolder,"/",f)
@@ -34,19 +44,23 @@ if (length(newfiles) > 0)
     psi <- trimws(psi)
     psi <- substr(psi,start = 1, stop = (nchar(psi) - 4))
     # get mouse number
-    mousenum <- trimws(df$V1[dim(df)[1]])
-    len <- nchar(mousenum)
-    mousenum <- substr(mousenum, start = len - 3, stop = len)
+    notes <- trimws(df$V1[dim(df)[1]])
+    len <- nchar(notes)
+    mousenum <- as.integer(substr(notes, start = len - 3, stop = len))
     newrownum <- dim(master)[1] + 1
-    master[newrownum,"Date"] <- thedate
+    master[newrownum,"Date"] <- difftime(thedate, "1899-12-30", units = "days") + 1
     master[newrownum,"Subject"] <- mousenum
-    master[newrownum,"BOP.(psi)"] <- psi
+    master[newrownum,"BOP (psi)"] <- psi
+    master[newrownum, "Notes"] <- notes
     # change name of file, so its not processed twice
     newf <- paste0(sub('\\.csv$','', bopfilepath),"processed.csv")
     print(paste("renaming",bopfilepath,"to",newf))
     file.rename(bopfilepath, newf)
   }
   # update xls and save it back out
-  writeData(wb, sheet = "Master", master, colNames = T)
-  saveWorkbook(wb, masterfilename, overwrite = T)
+  #writeData(wb, sheet = "Master", master, colNames = T)
+  #saveWorkbook(wb, masterfilename, overwrite = T)
+  write.xlsx(master, file = paste0(masterfilefolder,"/","newBops",nowstamp,".xlsx"))
+  write.csv(master, file = paste0(masterfilefolder,"/","newBops",nowstamp,".csv"),
+            row.names = FALSE, na="")
 }
